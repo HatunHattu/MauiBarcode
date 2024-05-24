599230c34cd386144070581683f2c50bd4b491cf
Aaro Inginmaa <77359720+AaroInginmaa@users.noreply.github.com>
2024-05-24 10:19:32 +0300

    Toimiva

---
+++ BarcodeReader/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ZXing.Net.Maui;
+
+namespace BarcodeReader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            cameraBarcodeReaderView.Options = new BarcodeReaderOptions
+            {
+                Formats = BarcodeFormats.OneDimensional,
+                AutoRotate = true,
+                Multiple = true
+            };
+
+        }
+
+        protected void BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+        {
+            foreach (var barcode in e.Results)
+            {
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    barcodeResult.Text = $"{barcode.Format}: {barcode.Value}";
+                });
+            }
+        }
+    }
+
+}
