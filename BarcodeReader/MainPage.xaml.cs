a72dd0a8d0da21a4137723015311ae0faa1f8ead
EinoTaimisto <eino.taimisto@outlook.com>
2024-05-24 10:58:57 +0300

    SQL kokeilua

--- BarcodeReader/MainPage.xaml.cs
+++ BarcodeReader/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using ZXing.Net.Maui;
+﻿using Data;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using ZXing.Net.Maui;
 
 namespace BarcodeReader
 {
@@ -25,6 +28,28 @@ namespace BarcodeReader
                 {
                     barcodeResult.Text = $"{barcode.Format}: {barcode.Value}";
                 });
+            }
+        }
+
+        public void ConnSQL()
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.Server = "loalchost";
+            dbCon.DatabaseName = "qrcodereader";
+            dbCon.UserName = "root";
+            dbCon.Password = "";
+            if (dbCon.IsConnect())
+            {
+                string query = "INSERT INTO barcodes";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(0);
+                    string someStringFromColumnOne = reader.GetString(1);
+                    Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                }
+                dbCon.Close();
             }
         }
     }
